5271c0b41de555ce3afb28928c2add2255e5d25a
fandy <huan.liu@frontpay.cn>
2017-02-21 14:21:16 +0800

    first commit

---
+++ CommandLine/CommandLine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CommandLine
+//
+//  Created by QianTuFD on 2017/2/21.
+//  Copyright © 2017年 fandy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ Objective-C/Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objective-C
+//
+//  Created by QianTuFD on 2017/2/21.
+//  Copyright © 2017年 fandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
