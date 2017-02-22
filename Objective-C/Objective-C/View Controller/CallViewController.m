f29aa1de6dc9e566a1a3c0ec99e5057fe6c19eef
fandy <huan.liu@frontpay.cn>
2017-02-22 16:35:20 +0800

    FlatButton_POP

--- Objective-C/Objective-C/View Controller/CallViewController.m
+++ Objective-C/Objective-C/View Controller/CallViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CallViewController.h"
+#import "FlatButton.h"
 
 @interface CallViewController ()
 
@@ -16,12 +17,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.view.backgroundColor = [UIColor blueColor];
+    [self addCallButtons];
 }
 
 - (void)addCallButtons {
+    UIButton *button = [FlatButton button];
+    [button setFrame:CGRectMake(0, 0, 200, 200)];
+    button.translatesAutoresizingMaskIntoConstraints = NO;
+    [button setTitle:@"ååå" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(touch) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:button
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.f
+                                                           constant:0.f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:button
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.f
+                                                           constant:0.f]];
     
 }
 
+- (void)touch {
+    
+}
 
 @end
--- Objective-C/Objective-C/View/FlatButton.m
+++ Objective-C/Objective-C/View/FlatButton.m
@@ -7,6 +7,7 @@
 //
 
 #import "FlatButton.h"
+#import <pop/POP.h>
 
 @implementation FlatButton
 
@@ -23,16 +24,57 @@
     return self;
 }
 
+#pragma mark - Instance methods
+
+- (UIEdgeInsets)titleEdgeInsets {
+    return UIEdgeInsetsMake(4.f, 28.f, 4.f, 28.f);
+}
+
+- (CGSize)intrinsicContentSize {
+    CGSize s = [super intrinsicContentSize];
+    
+    return CGSizeMake(s.width + self.titleEdgeInsets.left + self.titleEdgeInsets.right,
+                      s.height + self.titleEdgeInsets.top + self.titleEdgeInsets.bottom);
+}
+
+
+
 #pragma mark - Private instance methods
 
 - (void)setup {
     self.layer.cornerRadius = 4.f;
+    self.backgroundColor = self.tintColor;
+    self.titleLabel.font = [UIFont systemFontOfSize:20];
     [self setTitleColor:[UIColor whiteColor]
                forState:UIControlStateNormal];
-    self.titleLabel.font = [UIFont fontWithName:@""
-                                           size:22];
-    
+    [self addTarget:self action:@selector(scaleToSmall)
+   forControlEvents:UIControlEventTouchDown | UIControlEventTouchDragEnter];
+    [self addTarget:self action:@selector(scaleAnimation)
+   forControlEvents:UIControlEventTouchUpInside];
+    [self addTarget:self action:@selector(scaleToDefault)
+   forControlEvents:UIControlEventTouchDragExit];
+
 }
 
+- (void)scaleToSmall {
+    POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(0.95f, 0.95f)];
+    [self.layer pop_addAnimation:scaleAnimation forKey:@"layerScaleSmallAnimation"];
+}
+
+- (void)scaleAnimation {
+    POPSpringAnimation *scaleAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.velocity = [NSValue valueWithCGSize:CGSizeMake(3.f, 3.f)];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(1.f, 1.f)];
+    scaleAnimation.springBounciness = 18.f;
+    [self.layer pop_addAnimation:scaleAnimation
+                          forKey:@"layerScaleSpringAnimation"];
+}
+
+- (void)scaleToDefault {
+    POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(1.f, 1.f)];
+    [self.layer pop_addAnimation:scaleAnimation forKey:@"layerScaleDefaultAnimation"];
+}
 
 @end
