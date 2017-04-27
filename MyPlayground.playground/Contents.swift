21a80a285d41528a481a1189bd91dd00833764ab
fandy <huan.liu@frontpay.cn>
2017-04-27 16:24:15 +0800

    ab

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,19 @@
-//: Playground - noun: a place where people can play
+
 
 import UIKit
-import PlaygroundSupport
 
-PlaygroundPage.current.needsIndefiniteExecution = true
+struct Person {
+    var name: String
+}
+
+extension Person: Equatable {
+    static func ==(lhs: Person, rhs: Person) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
 
 
+Person(name: "Lisa") == Person(name: "Lisa") // → true
+Person(name: "Lisa") == Person(name: "Bart") // → false
 
-
-
+print("aaa")
